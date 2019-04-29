79e04f9bb33db325f560a9e7fe94736be3ae282e
Pierre-Champion <pierre.champion@bbox.fr>
2019-04-29 09:47:18 +0200

    inscription & confirmation

--- src/FormArmorBundle/Controller/ClientController.php
+++ src/FormArmorBundle/Controller/ClientController.php
@@ -26,6 +26,43 @@ class ClientController extends Controller
 
     public function indexAction()
     {
-        return $this->render('FormArmorBundle:Client:index.html.twig');
+        $user=$this->container->get('security.context')->getToken()->getUser();
+        $client_id = $user->id;
+        $em = $this->getDoctrine()->getManager();
+        $RAW_QUERY = 'call session_autorisee(:client_id)';
+        $statement = $em->getConnection()->prepare($RAW_QUERY);
+        $statement->bindValue('client_id', $client_id);
+        $statement->execute();
+        $RAW_QUERY = 'select s.id, f.libelle, niveau, description, date_debut as date from session_formation s, client c,
+         formation f where s.id in (select id from id_session_autorisee) and c.id = :client_id and f.id = s.formation_id';
+        $statement = $em->getConnection()->prepare($RAW_QUERY);
+        $statement->bindValue('client_id', $client_id);
+        $statement->execute();
+        $result = $statement->fetchAll();
+        return $this->render('FormArmorBundle:Client:index.html.twig', array("sessions" => $result));
+    }
+    public function inscripAction($id, Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $RAW_QUERY = 'select s.id, f.libelle, date_debut as date from session_formation s,
+        formation f where s.id = :id_session and f.id = s.formation_id';
+        $statement = $em->getConnection()->prepare($RAW_QUERY);
+        $statement->bindValue('id_session', $id);
+        $statement->execute();
+        $result = $statement->fetchAll();
+        return $this->render('FormArmorBundle:Client:inscription.html.twig', array("session" => $result));
+    }
+    public function confirmAction($id, Request $request)
+    {
+        $user=$this->container->get('security.context')->getToken()->getUser();
+        $client_id = $user->id;
+        $em = $this->getDoctrine()->getManager();
+        $RAW_QUERY = 'insert into inscription (client_id, session_formation_id, date_inscription, etat) values (:id_client, :id_session, :date_inscription, \'\')';
+        $statement = $em->getConnection()->prepare($RAW_QUERY);
+        $statement->bindValue('id_session', $id);
+        $statement->bindValue('id_client', $id_client);
+        $statement->bindValue('date_inscription', date("Y-m-d"));
+        $statement->execute();
+        return $this->redirectToRoute('form_armor_client');
     }
 }
